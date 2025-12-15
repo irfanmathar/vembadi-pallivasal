2a424de65e040ddfbfde26c5ab19e4c62702c11c
Mohamed Mathar Irfan <124813960+irfanmathar@users.noreply.github.com>
2025-12-16 00:41:53 +0530

    Update settings.py

--- djangoprojects/settings.py
+++ djangoprojects/settings.py
@@ -24,7 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cwu@v!fpjf018v(#mbm5@^f#q9dwh&(^ro0r3=nd&c!4m!%@p!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://vembadi-pallivasal-1.onrender.com",
+]
 
 ALLOWED_HOSTS = [
      "vembadi-pallivasal-1.onrender.com",
